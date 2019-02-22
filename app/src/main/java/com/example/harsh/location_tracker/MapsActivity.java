7a65e05f574f602e71233ec6019f4f96bd8fc761
harshagarwal16 <harshitagarwal059@gmail.com>
2019-02-23 00:14:03 +0530

    Added SOS button

--- app/src/main/java/com/example/harsh/location_tracker/MapsActivity.java
+++ app/src/main/java/com/example/harsh/location_tracker/MapsActivity.java
@@ -1,15 +1,27 @@
 package com.example.harsh.location_tracker;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,22 +30,50 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.os.Handler;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     LocationManager locationManager;
+    public double latitude,longitude;
+    public static int flag=1;
+    public void click()
+    {
+        getCoordinates();
+    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+    // Create the Handler object (on the main thread by default)
+    Handler handler = new Handler();
+    // Define the code block to be executed
+    private Runnable runnableCode = new Runnable() {
+        @Override
+        public void run() {
+            click();
+        }
+    };
+
+    public void getCoordinates()
+    {
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -50,15 +90,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
                 @Override
                 public void onLocationChanged(Location location) {
-                    double latitude = location.getLatitude();
-                    double longitute  = location.getLongitude();
-                    LatLng latlng = new LatLng(latitude,longitute);
+                    latitude = location.getLatitude();
+                    longitude  = location.getLongitude();
+                    LatLng latlng = new LatLng(latitude,longitude);
                     Geocoder geocoder = new Geocoder(getApplicationContext());
                     try {
-                        List<Address> addressList = geocoder.getFromLocation(latitude,longitute,1);
-                        String  str = addressList.get(0).getCountryName()+","+addressList.get(0).getLocality();
-                        mMap.addMarker(new MarkerOptions().position(latlng).title(str));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng,15));
+                        List<Address> addressList = geocoder.getFromLocation(latitude,longitude,1);
+                        String  str = addressList.get(0).getLocality()+","+addressList.get(0).getCountryName()+",("+latitude+","+longitude+")";
+
+                        if(flag==1) {
+                            save(latlng,str);
+
+                        }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -85,15 +128,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
                 @Override
                 public void onLocationChanged(Location location) {
-                    double latitude = location.getLatitude();
-                    double longitute  = location.getLongitude();
-                    LatLng latlng = new LatLng(latitude,longitute);
+                    latitude = location.getLatitude();
+                    longitude  = location.getLongitude();
+                    LatLng latlng = new LatLng(latitude,longitude);
                     Geocoder geocoder = new Geocoder(getApplicationContext());
                     try {
-                        List<Address> addressList = geocoder.getFromLocation(latitude,longitute,1);
-                        String  str = addressList.get(0).getCountryName()+","+addressList.get(0).getLocality();
+                        List<Address> addressList = geocoder.getFromLocation(latitude,longitude,1);
+                        String  str = addressList.get(0).getLocality()+","+addressList.get(0).getCountryName()+",("+latitude+","+longitude+")";
                         mMap.addMarker(new MarkerOptions().position(latlng).title(str));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng,15));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng,16));
+                        if(flag==1) {
+                            save(latlng,str);
+
+                        }
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -115,8 +162,191 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             });
         }
-
     }
+    public void save(LatLng latLng,String str2)
+    {
+        try {
+            Calendar calender = new Calendar() {
+                @Override
+                protected void computeTime() {
+
+                }
+
+                @Override
+                protected void computeFields() {
+
+                }
+
+                @Override
+                public void add(int field, int amount) {
+
+                }
+
+                @Override
+                public void roll(int field, boolean up) {
+
+                }
+
+                @Override
+                public int getMinimum(int field) {
+                    return 0;
+                }
+
+                @Override
+                public int getMaximum(int field) {
+                    return 0;
+                }
+
+                @Override
+                public int getGreatestMinimum(int field) {
+                    return 0;
+                }
+
+                @Override
+                public int getLeastMaximum(int field) {
+                    return 0;
+                }
+            }.getInstance();
+            SimpleDateFormat dateFormat=new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            final String strDate = dateFormat.format(calender.getTime());
+            // Creates a file in the primary external storage space of the
+            // current application.
+            // If the file does not exists, it is created.
+            mMap.addMarker(new MarkerOptions().position(latLng).title(str2+" "+strDate));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,20));
+            String newLine = System.getProperty("line.separator");
+            String str= Double.toString(latLng.latitude)+","+Double.toString(latLng.longitude);
+            File testFile = new File(this.getExternalFilesDir(null), "locationCoordinates.txt");
+            if (!testFile.exists())
+                testFile.createNewFile();
+            //str+= ","+DateFormat.getDateTimeInstance().format(new Date())+newLine;
+            str+=" "+strDate+newLine;
+            // Adds a line to the file
+            BufferedWriter writer = new BufferedWriter(new FileWriter(testFile, true /*append*/));
+            writer.write(str);
+            writer.close();
+            // Refresh the data so it can seen when the device is plugged in a
+            // computer. You may have to unplug and replug the device to see the
+            // latest changes. This is not necessary if the user should not modify
+            // the files.
+            MediaScannerConnection.scanFile(this,
+                    new String[]{testFile.toString()},
+                    null,
+                    null);
+        } catch (IOException e) {
+            Log.e("ReadWriteFile", "Unable to write to the TestFile.txt file.");
+        }
+    }
+
+    public void button(View view) {
+        getCoordinates();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        handler.postDelayed(runnableCode,2000);
+    }
+
+
+    private void sendSMS(String phoneNumber, String message) {
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0, new Intent(
+                SENT), 0);
+
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(DELIVERED), 0);
+
+        // ---when the SMS has been sent---
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        ContentValues values = new ContentValues();
+                            values.put("address", "9149297427");// txtPhoneNo.getText().toString());
+                            values.put("body", "fuck me!!!!!!!!!!!");
+                        getContentResolver().insert(
+                                Uri.parse("content://sms/sent"), values);
+                        Toast.makeText(getBaseContext(), "SMS sent",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(), "Generic failure",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(), "No service",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getBaseContext(), "Null PDU",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getBaseContext(), "Radio off",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SENT));
+
+        // ---when the SMS has been delivered---
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getBaseContext(), "SMS not delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(DELIVERED));
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
+    }
+    public void smsSendMessage(View view) {
+        Geocoder geocoder;
+        List<Address> addresses = null;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+
+        Intent smsVIntent = new Intent(Intent.ACTION_VIEW);
+        // prompts only sms-mms clients
+        smsVIntent.setType("vnd.android-dir/mms-sms");
+
+        // extra fields for number and message respectively
+        //smsVIntent.putExtra("address", "9149297427");
+        smsVIntent.putExtra("sms_body", address+"\n"+"\nLatitude : "+latitude+ "\nLongitude : "+longitude);
+        try{
+            startActivity(smsVIntent);
+        } catch (Exception ex) {
+            Toast.makeText(this, "Your sms has failed...",
+                    Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
+    }
+
 
 
     /**
@@ -131,10 +361,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
-        //LatLng roorkee = new LatLng(29.877562, 77.879957);
+        LatLng roorkee = new LatLng(29.877562, 77.879957);
         //mMap.addMarker(new MarkerOptions().position(roorkee).title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(roorkee,3));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(roorkee,3));
     }
 }
